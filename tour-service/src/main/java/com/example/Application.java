be54949e11b4a2a6283fd554b21be3f26431270d
sathish-chanda <sathishchandas@gmail.com>
2025-05-19 19:16:01 -0400

    @Service and @Repository annotation will be used for the dependency injection with annotation. This commit is the solution of the  challenge exercise to add a new tour.

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.example.tourservice.TourManagementService;
 import com.example.tourservice.TravelAgentService;
 
 public class Application {
@@ -10,6 +11,8 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class.getPackageName());
         TravelAgentService agent = context.getBean(TravelAgentService.class);
+        TourManagementService tourMgmtService = context.getBean(TourManagementService.class);
+        tourMgmtService.createTour("Zoo Tour", 100, true);
         System.out.println("\n******Explore California Tour Catalogue******");
         agent.displayTours();
 
