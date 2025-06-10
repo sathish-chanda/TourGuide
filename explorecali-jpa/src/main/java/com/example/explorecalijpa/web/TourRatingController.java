faa6301d0102b6f85542be536110db54ec526e37
sathish-chanda <sathishchandas@gmail.com>
2025-06-10 08:36:35 -0400

    Added put,patch and delete end points.

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
@@ -1,18 +1,22 @@
 package com.example.explorecalijpa.web;
 
 import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.example.explorecalijpa.business.TourRatingService;
 import com.example.explorecalijpa.model.TourRating;
@@ -46,6 +50,21 @@ public class TourRatingController {
   @GetMapping("/average")
   public Map<String, Double> getAverage(@PathVariable(value = "tourId") int tourId) {
     return Map.of("average",tourRatingService.getAverageScore(tourId));
+  }
+
+  @PutMapping
+  public RatingDTO updateWithPut(@PathVariable(value = "tourId") int tourId,@RequestBody @Valid RatingDTO ratingDTO) {
+    return new RatingDTO(tourRatingService.update(tourId,ratingDTO.getCustomerId(), ratingDTO.getScore(), ratingDTO.getComment()));
+  }
+ 
+  @DeleteMapping("{customerId}")
+  public void delete(@PathVariable(value = "tourId") int tourId,@PathVariable(value = "customerId") int customerId) {
+    tourRatingService.delete(tourId, customerId);
+  }
+
+  @PatchMapping
+  public RatingDTO updateSomeWithPatch(@PathVariable(value = "tourId") int tourId, @RequestBody @Valid RatingDTO ratingDTO) {
+    return new RatingDTO(tourRatingService.updateSome(tourId, ratingDTO.getCustomerId(), Optional.ofNullable(ratingDTO.getScore()), Optional.ofNullable(ratingDTO.getComment()))); 
   }
 
   @ExceptionHandler(NoSuchElementException.class)
