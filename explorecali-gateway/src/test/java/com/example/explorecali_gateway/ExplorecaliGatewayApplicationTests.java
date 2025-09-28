2f493924ebc23e5d419e836e8698fc9e005afd6d
sathish-chanda <sathishchandas@gmail.com>
2025-09-28 19:56:19 -0400

    Added Gateway micro service.

---
+++ explorecali-gateway/src/test/java/com/example/explorecali_gateway/ExplorecaliGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.explorecali_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorecaliGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
